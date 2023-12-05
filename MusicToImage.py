b22c48643c5ff3d8ffd78753a87aa4d90df12eb2
shreshta-y <shreshtyadav245@gmail.com>
2023-12-04 20:20:49 -0500

    Update MusicToImage.py

--- MusicToImage.py
+++ MusicToImage.py
@@ -118,78 +118,18 @@ def generate(payload, style, iterations):
     return output
             
 
-def main():
-    print("main call")
-    input = """
-Mmm, ooh, ooh, ooh, ooh, ooh, ooh, ooh, ooh
-Mama's bailing down that road, craving 9021
-She a porn star girl, oh, from the valley (honestly, God bless)
-Who left her hometown world all for that alley
-Oh, created Lake Tahoe all from her panties
-(I hope it was wet like my jumper, though)
-Ooh, used to take the long way home, long way home, all for that candy
-Baby's hooked on feeling low
-Do, do, do
-Do, do
-Jacques turned La Flame, now he rolling on an Addy
-Fifty on a chain, 'nother fifty on a Caddy, ooh
-He might pop him a pill, pop him a seal, pop anyone
-Pop anything, pop anything to find that alley
-Hmm, yeah, to find that alley (mmm)
-Baby's hooked on feeling low
-Do, do, do
-Do, do
-In that 90210, 90210, looking for that alley
-In the 90210, 90210, looking for that alley, ooh
-It's the superstar girl (baby's hooked)
-Superstar girl, roaming in that alley (on feeling low)
-Oh, in the 90210, 90210, somewhere in that alley
-Ooh-ah
-Ooh
-Ooh-ooh-ooh-ooh
-Yeah
-My granny called, she said, "Travvy, you work too hard
-I'm worried you forget about me"
-I'm falling in and out of clouds, don't worry, I'ma get it, granny, uh
-What happened? Now my daddy happy, mama called me up
-That money coming and she love me, I done made it now
-I done found life's meaning now, all them her heart'd break
-Her heart not in pieces now
-Friends turning into fraud niggas
-Practicing half the passion, you niggas packaged different
-All you niggas, you niggas want the swag, you can't have it
-I'ma sell it, you niggas salary 'bout to cap, bitch
-Youngest nigga out of Houston at the Grammys
-Smiling at 'em laughing at me
-I passed the rock to Ye, he pump faked and passed it back, bitch
-All of this off of rapping, should've wrote this in Latin
-Yeah, yeah
-Mmm, I know, I know, I know, I know, I know
-I know, I know, I know, I know, I know
-Cuzzo said we in the store, yeah, we 'bout to drop a four
-He passed the cigarette, I choke, woo
-Told my auntie put them 'Ports down, them 'Ports down
-Now you know you love your own now
-Hit the stage, they got their hands up, don't put your nose down
-I ain't knockin', nigga, I knocked the door down, for sure now
-Whole crew, I swear they counting on me
-Gold chains, gold rings, I got an island on me
-Houses on me, he got them ounces on him
-Holy Father, come save these niggas, I'm styling on 'em
-Good Lord, I see my good fortune in all these horses
-I'm driving too fast to stop, so all these signs, I ignore them
-Just this guy from north of the border, my chips is in order
-My mom's biggest supporter so now a nigga support her, nigga
-"""
-    #print("Token size: ", len(input)/4)
-    #print(lyrics)
-    #print(len(input))
-    out = generate(input, None, 4)
-    print(out)
-    quit()
+#def main():
+#    print("main call")
+#    input = """"""
+#    #print("Token size: ", len(input)/4)
+#    #print(lyrics)
+#    #print(len(input))
+#    out = generate(input, None, 4)
+#    print(out)
+#    quit()
     
-if __name__ == "__main__":
-    main()
+#if __name__ == "__main__":
+#    main()
 
 
 
